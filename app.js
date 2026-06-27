a513730369a7855df0fa5e25d5d211d67753fb98
morphyatina <glebashks64@gmail.com>
2026-06-27 17:24:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,15 @@ import { timerRef, onSnapshot } from "./firebase.js";
 
 const timer = document.getElementById("timer");
 
-let endTime = 0;
-
 onSnapshot(timerRef, (doc) => {
-    if (doc.exists()) {
-        endTime = doc.data().endTime || 0;
-    }
+    if (!doc.exists()) return;
+
+    const data = doc.data();
+    let seconds = data.seconds || 0;
+
+    let m = Math.floor(seconds / 60);
+    let s = seconds % 60;
+
+    timer.innerHTML =
+        String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0");
 });
-
-function update() {
-    const now = Date.now();
-    let left = Math.max(0, Math.floor((endTime - now) / 1000));
-
-    let m = Math.floor(left / 60);
-    let s = left % 60;
-
-    timer.textContent =
-        String(m).padStart(2, "0") +
-        ":" +
-        String(s).padStart(2, "0");
-}
-
-setInterval(update, 100);
-update();
